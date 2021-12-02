333dc9c6b4c5a9b3df9683c515e92432037f7548
理由新し <moeetomoni@outlook.com>
2021-12-02 22:13:19 +0100

    feat(db): adding models & migrations & request:
    Table:
      - Decoration
      - Rescuer
      - Castawayboat
      - Category

---
+++ database/migrations/2021_12_02_201857_create_decorations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDecorationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('decorations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->string('label');
+            $table->string('image')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('decorations');
+    }
+}
