4b8b2f85d630c1b08de30aee7eb4cdfd3fcc6cd7
sven portable linux perso <sven.remond.sti2@gmail.com>
2021-12-02 22:44:19 +0100

    Add all controller

---
+++ app/Http/Controllers/TestimonyController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Testimony;
+use Illuminate\Http\Request;
+
+class TestimonyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Testimony  $testimony
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Testimony $testimony)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Testimony  $testimony
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Testimony $testimony)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Testimony  $testimony
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Testimony $testimony)
+    {
+        //
+    }
+}
