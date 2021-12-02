7f73cbb1c20b03bbabc8541ac820c37695dba469
sven portable linux perso <sven.remond.sti2@gmail.com>
2021-12-03 00:11:42 +0100

    [ADD] seeder for category

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::factory()->count(20)->create();
+    }
+}
