5701b0aa6514a618a188f8bbd1d88f977b48e0f0
理由新し <moeetomoni@outlook.com>
2021-12-03 03:50:56 +0100

    fix(factory): Case error

--- database/factories/CastawayBoatFactory.php
+++ database/factories/CastawayBoatFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class CastawayboatFactory extends Factory
+class CastawayBoatFactory extends Factory
 {
     /**
      * Define the model's default state.
